723621bf1adbe20cc4d6147dfe98df7674e682d9
Ksanter <ksantosx1987@gmail.com>
2025-10-11 13:28:35 +0300

    Добавьте файлы проекта.

---
+++ 01_Intro_to/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Intro_to
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
